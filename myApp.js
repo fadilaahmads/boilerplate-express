a072001b4e6091909ffc2fb5eb869df70195503b
fadilaahmads <fadilahmad59@gmail.com>
2021-10-29 17:03:26 +0700

    Add method use the .env file, and commented out serve json on specific route method

--- myApp.js
+++ myApp.js
@@ -19,9 +19,20 @@ app.get('/', (req, res) => {
 app.use('/public', express.static(__dirname + '/public/'));
 
 //Serve JSON on a Specific Route
-app.get('/json', (req, res) => {
+/*app.get('/json', (req, res) => {
     res.json({ "message": "Hello json" });
-});
+});*/
+
+//Use the .env File
+app.get('/json', (req, res) => {
+    var response;
+    if(process.env.MESSAGE_STYLE === 'uppercase'){
+       response = "Hello json".toUpperCase();
+    }else{
+       response = "Hello json";
+    }
+    res.json({ "message": response });
+  });
   
 
 
