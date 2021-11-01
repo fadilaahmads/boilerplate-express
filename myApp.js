733ba205dfdaf45f9212805dd3dc75a442bcfe39
fadilaahmads <fadilahmad59@gmail.com>
2021-11-01 22:38:27 +0700

    Change each task comments and adding number into them

--- myApp.js
+++ myApp.js
@@ -1,29 +1,29 @@
 var express = require('express');
 var app = express();
 
-//Meet the Console
+/** 1) Meet the Console */
 //console.log('Hello World');
 
-//Start a Working Express Server
+/** 2) Start a Working Express Server */
 /*app.get('/', (req, res) => {
     res.send('Hello Express'); //it can send files too
 });*/
 
-//Serve an HTML files
+/** 3) Serve an HTML files */
 absolutePath = __dirname + '/views/index.html';
 app.get('/', (req, res) => {
   res.sendFile(absolutePath);
 });
 
-//Serve Static AssetsPassed
+/** 4) Serve Static Assets */
 app.use('/public', express.static(__dirname + '/public/'));
 
-//Serve JSON on a Specific Route
+/** 5) Serve JSON on a Specific Route */
 /*app.get('/json', (req, res) => {
     res.json({ "message": "Hello json" });
 });*/
 
-//Use the .env File
+/** 6) Use the .env File */
 /*app.get('/json', (req, res) => {
     var response;
     if(process.env.MESSAGE_STYLE === 'uppercase'){
@@ -34,14 +34,14 @@ app.use('/public', express.static(__dirname + '/public/'));
     res.json({ "message": response });
   });*/
   
-//Implement a Root-Level Request Logger Middleware
+/** 7) Implement a Root-Level Request Logger Middleware */
 app.use(function middleware(req, res, next) {
   const response = req.method+' '+req.path+' - '+req.ip;
   console.log(response);
   next();
 });
 
-//Chain Middleware to Create a Time Server
+/** 8) Chain Middleware to Create a Time Server */
 app.get('/now', (req, res, next) => {
   req.time = new Date().toString();
   next();
@@ -51,13 +51,13 @@ app.get('/now', (req, res, next) => {
   });
 });
 
-//Get Route Parameter Input from the Client
+/** 9) Get Route Parameter Input from the Client */
 app.get('/:word/echo', function(req, res){
   const word = req.params.word;
   res.json({ echo: word});
 });
 
-//Get Query Parameter Input from the Client
+/** 10) Get Query Parameter Input from the Client */
 app.get('/name', (req, res) => {
   var firstName = req.query.first;
   var lastName = req.query.last;
@@ -67,11 +67,11 @@ app.get('/name', (req, res) => {
   });
 });
 
-//Use body-parser to Parse POST Requests
+/** 11) Use body-parser to Parse POST Requests */
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
+/** 12) Get Data from POST Requests */
 
 
 
